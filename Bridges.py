4155a46cdaffd35c930714cad5b955563d63976b
Srikar-ai <70573179+Srikar-ai@users.noreply.github.com>
2021-08-08 00:30:19 +0530

    Update Bridges.py

--- Bridges.py
+++ Bridges.py
@@ -1,28 +1,25 @@
 V=5
-graph=[[1,2,3],[0,2],[0,1],[0,4],[3]]
 visited=set()
-disc=[float('inf')]*V
-low=[float('inf')]*V
+graph=[[1,2,3],[0,2],[0,1],[0,4],[3]]   
+discovery=[float('inf')]*V
+low_link_value=[float('inf')]*V
 parent=[-1 for x in range(V)]
 time=0
-def dfs(node):
+def Find_Bridges(u):
     global time
-    visited.add(node)
-    disc[node]=time
-    low[node]=time
+    visited.add(u)
+    discovery[u]=time
+    low_link_value[u]=time
     time+=1
-    for x in graph[node]:
-        if x not in visited:
-            parent[x]=node
-            dfs(x)
-            low[node]=min(low[node],low[x])
-            if low[x]>disc[node]:
-                print('bridge...',x,node)
-        elif x!=parent[node]:
-            low[node]=min(low[node],disc[x])
-
-
+    for v in graph[u]:
+        if v not in visited:
+            parent[v]=u
+            Find_Bridges(v)
+            low_link_value[u]=min(low_link_value[u],low_link_value[v])
+            if low_link_value[v]>discovery[u]:
+                print('There is Bridge :',u,v)
+        elif v!=parent[u]:
+            low_link_value[u]=min(low_link_value[u],discovery[v])
 
 for x in range(V):
-    dfs(x)
-print(parent)
+    Find_Bridges(x)
